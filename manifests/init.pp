57541774194d5039773a9dc8f822b23fdb21b5ec
Ashish Jaiswal <ashish@obmondo.com>
2017-09-01 08:48:41 +0200

    bunch of improvements
    1. Array of hashes seems to be a bad idea. So converted into hash with Struct so we are limited to the type
    2. Ubuntu package was not support, so added support for it
    3. Service file had some bug with ip link up command was missing altogether
    4. Service was getting reload if the file change, but seems like it does not work with template, since was not getting changed.
    
    Sample Config
    
    ```
    wireguard::tunnels:
      wg0:
        private_key: 'xyz'
        listen_port: 51820
        virtual_ip: 192.168.2.1/24
        peers:
          external-api01:
            public_key: 'efg'
            allowed_ips: 192.168.2.2/32
            endpoint: abc.com:51820
            persistent_keepalive: 10
          wordpress:
            public_key: 'abc'
            allowed_ips: 192.168.2.3/32
            endpoint: abc.com51822
            persistent_keepalive: 10
    ```

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,35 @@
 class wireguard (
-  Array[Hash] $tunnels = [],
-  Array[Hash] $simple_tunnels = [],
+  Hash[String, Struct[
+    {
+      ensure      => Optional[String],
+      private_key => String,
+      listen_port => Integer,
+      virtual_ip  => String,
+      peers       => Hash,
+    }
+  ]] $tunnels = {},
+  Hash[String, Struct[
+    {
+      ensure           => Optional[String],
+      private_key      => String,
+      listen_port      => Integer,
+      virtual_ip       => String,
+      peer_public_key  => String,
+      peer_allowed_ips => Optional[String],
+      peer_endpoint    => Optional[String],
+    }
+  ]] $simple_tunnels = [],
 ) {
   include wireguard::packages
 
-  $tunnels.each |$tunnel| {
-    $tunnel.each |$name, $params| {
-      wireguard::tunnel { $name:
-        * => $params,
-      }
+  $tunnels.each |$name, $params| {
+    wireguard::tunnel { $name:
+      * => $params,
     }
   }
-  $simple_tunnels.each |$simple_tunnel| {
-    $simple_tunnel.each |$name, $params| {
-      wireguard::simple_tunnel { $name:
-        * => $params,
-      }
+  $simple_tunnels.each |$name, $params| {
+    wireguard::simple_tunnel { $name:
+      * => $params,
     }
   }
 }
--- manifests/simple_tunnel.pp
+++ manifests/simple_tunnel.pp
@@ -2,28 +2,39 @@
 # peer. It uses wireguard::tunnel internally.
 #
 # @param private_key the private key used here
+#
 # @param listen_port on which port wireguard should listen for incoming
 # connections. Chosen randomly if not specified
+#
 # @param peer_public_key The public key of the one and only peer @param
-# peer_allowed_ips A comma-separated list of ip (v4 or v6) addresses with CIDR
+#
+# @param virtual_ip virtual network also needs an IP address for each node so
+# that machines can communicate between each other over IP
+#
+# @param peer_allowed_ips A comma-separated list of ip (v4 or v6) addresses with CIDR
 # masks from which this peer is allowed to send incoming traffic and to which
 # outgoing traffic for this peer is directed. Defaults to 0.0.0.0/0 and ::/0.
+#
 # @param peer_endpoint An  endpoint  IP or hostname, followed by a colon, and
 # then a port number. This endpoint will be updated automatically to the most
 # recent source IP address and port of correctly authenticated packets from the
 # peer. Optional.
 
 define wireguard::simple_tunnel (
+  String  $ensure                    = 'present',
   String  $private_key,
   Integer $listen_port,
-  String $peer_public_key,
+  String  $peer_public_key,
+  String  $virtual_ip,
   Optional[String] $peer_allowed_ips = undef,
-  Optional[String] $peer_endpoint = undef,
+  Optional[String] $peer_endpoint    = undef,
 ) {
 
   wireguard::tunnel { $title:
+    ensure      => $ensure,
     private_key => $private_key,
     listen_port => $listen_port,
+    virtual_ip  => $virtual_ip,
     peers       => [{
       public_key  => $peer_public_key,
       allowed_ips => $peer_allowed_ips,
